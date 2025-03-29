3c67d7395f4cb2bf88b6a9c9df1f107703aed14e
KarloSiric <ksiric99@gmail.com>
2025-03-29 14:43:21 +0100

    solved rna_transcription problem for exercism....

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -2,12 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+char converting_rnas(char); // function definitions
+
+char converting_rnas(char c) {
+    switch(c) {
+        case 'A':
+            return 'U';
+        case 'C':
+            return 'G';
+        case 'G':
+            return 'C';
+        case 'T':
+            return 'A';
+        default:
+            return '\0';
+    }
+}
+
 char *to_rna(const char *dna) {
 
-    size_t length = strlen(dna) + 1;
+    size_t length = strlen(dna);
     char *rna = malloc(length);
-    int i;
-    for (i = 0; dna[i]; i++) {
-        
-   }
+    memset(rna, '\0', length);
+
+    for (int i = 0; dna[i]; i++) {
+        rna[i] = converting_rnas(dna[i]);
+        if (!rna[i]) {
+            free(rna);
+            return NULL;
+        }
+    }
+
+    return rna;
 }
