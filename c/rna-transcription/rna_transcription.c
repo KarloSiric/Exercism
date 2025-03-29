b6942282acf72b88262150a3379624249555ff5d
KarloSiric <ksiric99@gmail.com>
2025-03-29 14:33:53 +0100

    Adding solved space age.. working now on rna problem...

---
+++ c/rna-transcription/rna_transcription.c
@@ -0,0 +1,13 @@
+#include "rna_transcription.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *to_rna(const char *dna) {
+
+    size_t length = strlen(dna) + 1;
+    char *rna = malloc(length);
+    int i;
+    for (i = 0; dna[i]; i++) {
+        
+   }
+}
--- c/space-age/space_age.c
+++ c/space-age/space_age.c
@@ -1,6 +1,29 @@
 #include "space_age.h"
 
-float age(planet_t p, int64_t s) {
-    
+/* One Earth year equals 365.25 Earth days, or 31,557,600 seconds */
 
+/* So we need to find the seconds on some other planet that will be given to us */
+
+const float EARTH_YEAR_IN_SECONDS = 31557600;
+
+const float orbital_periods[] = { // here we make an array of all those orbits!
+    0.2408467,
+    0.61519726,
+    1.0,
+    1.8808158,
+    11.862615,
+    29.447498,
+    84.016846,                   
+    164.79132
+};
+
+float age(planet_t p, int64_t s) {
+
+    if (p < 0 || p > 7) {
+        return -1;
+    }
+
+
+    float years = s / orbital_periods[p] / EARTH_YEAR_IN_SECONDS; 
+    return years;
 }
