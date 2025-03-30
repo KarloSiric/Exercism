830eee4e225bae1455e64cbe545375f69085718f
KarloSiric <ksiric99@gmail.com>
2025-03-30 14:05:11 +0200

    Adding all of the changes for programming solving raindrops in exercism...

--- c/two-fer/two_fer.c
+++ c/two-fer/two_fer.c
@@ -1,1 +1,12 @@
 #include "two_fer.h"
+#include <stdio.h>
+
+void two_fer(char *buffer, const char *name) {
+    
+    if (name == NULL) {
+        sprintf(buffer, "One for you, one for me.");
+    } else {
+        sprintf(buffer, "one for %s, one for me.", name);
+    } 
+
+}
